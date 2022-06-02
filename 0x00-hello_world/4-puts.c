f951eecabf46d17f4acc8513733dadd29b159f2e
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-06-02 14:53:48 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-char *word="Programming is like building a multilingual puzzle\n";
+char *word="\"Programming is like building a multilingual puzzle\n";
 puts(word);
 return (0);
 }
