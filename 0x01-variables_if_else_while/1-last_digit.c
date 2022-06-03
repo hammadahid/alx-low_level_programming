9a1e3ab890ec041b6cabedfe069ebfa101e72e85
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-06-03 23:34:59 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,27 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <time.h>
+#include <stdlib.h>
 /**
- * main - Performs Different conditional operations on the n
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
  */
 int main(void)
 {
 	int n;
-int Last_Digit;	
+	int last;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-Last_Digit = n % 10;
-printf("Last digit of %d is %d ", n, Last_Digit);
-if (Last_Digit > 5)
-{
-printf("and is greater than 5\n");
+	last = n % 10;
+
+	if (last > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %i is %i and is 0\n", n, last);
+	else if (last < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+
+	return (0);
 }
-if (Last_Digit == 0)
-{
-printf("and is 0\n");
-}
-if (Last_Digit < 5 && Last_Digit != 0)
-{
-printf("and is less than 6 and not 0\n");
-}
-return (0);
-}
+
