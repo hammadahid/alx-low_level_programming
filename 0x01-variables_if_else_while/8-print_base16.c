66290f43acf22bced130554f5acb3a3ca632293b
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-06-03 23:21:33 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,18 +5,19 @@
  *
  * Return: Always 0 (Success)
  */
-char a = 48;
-int i = 97;
+
 int main(void)
 {
-    for (; i <= 102 ; i++)
-    {
-        putchar(i);
-    }
-    for(; a < 58 ; a++)
-    {
-        putchar(a);
-    }
-    putchar('\n');
-    return (0);
+char a = 48;
+int i = 97;
+for ( ; i <= 102 ; i++)
+{
+putchar(i);
+}
+for ( ; a < 58 ; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
 }
