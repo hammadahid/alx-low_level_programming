7e065edefc8c12a8d5f281efb7aec7337d1441b1
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-06-03 22:54:39 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,9 +5,10 @@
  * Description: Get a random number and check its last digit, compare it with 5
  * Return: 0
  */
-char a = 97;
+
 int main(void)
 {
+char a = 97;
 for ( ; a <= 122 ; a++)
 {
 putchar(a);
