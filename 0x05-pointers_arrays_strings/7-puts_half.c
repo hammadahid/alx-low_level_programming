281b4d6d5853a28e66a410eb1105a8abfaa887d3
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-06-13 22:25:07 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * puts_half - prints the second half of a string
+ * @str: pointer to char
  * Return: void
  */
 
@@ -12,15 +13,12 @@ while (str[i] != '\0')
 {
 i++;
 }
-
 n = i / 2;
-
 if ((i % 2 ) != 0)
 {
 n = (i -1) / 2;
 }
-
-for (n; n <= i; n++)
+for (n = 0; n <= i; n++)
 {
 _putchar(str[n]);
 }
