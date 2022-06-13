7181043ba8f9138ae2da96ddad6df5c4cae32396
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-06-13 22:22:02 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,13 +3,13 @@
 /**
  * print_array - print n elements of an array of integers
  * @a: pointer to char
- * @b: int 
+ * @b: int number of element 
  * Return: void
  */
 void print_array(int *a, int n)
 {
 int i = 0;
-for ( i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 if (i == (n - 1))
 {
