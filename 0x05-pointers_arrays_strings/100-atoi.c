e61bc2982d2a9133c03ebfe67cc2cbe2e408939b
SiruRoah <hammadahidii@gmail.com>
2022-06-14 20:38:05 +0100

    _atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,43 @@
-#include "100-atoi.h"
+#include "main.h"
 
-
+/**
+ * _atoi - convert string to int
+ * @s: pointer to char
+ * Return: int
+ */
+int _atoi(char *s)
+{
+int i = 0;
+int j = 0;
+int n = 0;
+int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+while (1)
+{
+if (i == len)
+{
+break;
+}
+if (s[i] >= 48 && s[i] <= 57)
+{
+n = (n * 10) + (s[i] - 48);
+}
+if (s[i] == 43)
+{
+j = j + 1;
+}
+if (s[i] == 45)
+{
+j = j + 2;
+}
+i++;
+}
+if (j == 2)
+{
+n = n * (-1);
+}
+return (n);
+}
