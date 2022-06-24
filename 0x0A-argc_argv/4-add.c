0b880f46703922d4a835124871fa016d92bab8d8
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-06-24 19:17:20 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -30,6 +30,12 @@ int main(int argc, char **argv)
 int sum = 0;
 int check = 0;
 int i = 0;
+if (argc == 1)
+{
+printf("0\n");
+}
+else
+{
 for (i = 0; i < argc; i++)
 {
 check = IsDigit(argv[i]);
@@ -45,6 +51,7 @@ printf("%d\n", sum);
 else
 {
 printf("Error\n");
+}
 }
 return (0);
 }
