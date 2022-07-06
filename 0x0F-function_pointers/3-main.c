0e9a0d27671e1bdf49c2e58bd562ca07e30d099f
SiruRoah <hammadahidii@gmail.com>
2022-07-06 22:44:22 +0100

    function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - get operation
+ * @argc - int
+ * @argv - double pointer
+ * Return:  */
+
+int main(int argc, char **argv)
+{
+int a = atoi(argv[1]);
+char *operator = argv[2];
+int b = atoi(argv[3]);
+int ptr;
+if (argc != 4)
+{
+exit (0);
+}
+ptr = get_op_func(operator)(a,b);
+printf("%d\n",ptr);
+return (0);
+}
