6a69c78cac20707fef1c943f34a9b563247db99e
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-07-07 19:35:53 +0100

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,34 +1,31 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
+
 /**
- * print_strings - prints string followed by new line
- * @separator: string
- * @n: const unsigned int
- * Return: null
+ * print_strings - prints strings, followed by a new line
+ * @separator: delimiter
+ * @n: n args
+ * Return: void
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-va_list arg;
-unsigned int i;
-if (separator == NULL)
-{
-return;
-}
-va_start(arg, n);
-for (i = 0; i < n; i++)
-{
-char *word = va_arg(arg, char *);
-if (word == NULL)
-{
-printf("nil");
-}
-else
-{
-printf("%s", word);
-}
-if (i != (n - 1))
-printf("%s", separator);
-}
-va_end(arg);
-printf("\n");
+	va_list arguments;
+	unsigned int s;
+	char *clone;
+
+	va_start(arguments, n);
+
+	for (s = 0; s < n; s++)
+	{
+		clone = va_arg(arguments, char*);
+		if (clone != NULL)
+			printf("%s", clone);
+		else
+			printf("%p", clone);
+		if (separator != NULL && s < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(arguments);
 }
