0b7b33fe2f6834ae4b5c7e0b690d01986086fee0
SiruRoah <84606611+hammadahid@users.noreply.github.com>
2022-07-14 16:05:59 +0100

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-global _start
-
-section .text
-
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, msglen
-    syscall
-
-    mov rax, 60
-    mov rdi, 0
-    syscall
-
-section .rodata
-msg:    db "Hello, Holberton", 10
-msglen: equ $ - msg
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
