3191e7dcd7212d3f4e1fbbce63af8462e3f38b31
SiruRoah <hammadahidii@gmail.com>
2022-07-14 16:00:38 +0100

    first advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_word - prints a word
+ * Return: void
+ */
+
+void __attribute__((constructor)) print_word()
+{
+printf(
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
