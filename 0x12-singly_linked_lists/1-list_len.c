26df5771fa418d99edebebdfc5634921f3214455
SiruRoah <hammadahidii@gmail.com>
2022-07-14 08:17:03 +0100

    Second task - lenght of list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - returns the lenght of a list
+ * @h: pointer to a struct
+ * Return:  lenght of list
+ */
+
+size_t list_len(const list_t *h)
+{
+int len = 0;
+while(h != NULL)
+{
+len++;
+h = h->next;
+}
+return (len);
+}
