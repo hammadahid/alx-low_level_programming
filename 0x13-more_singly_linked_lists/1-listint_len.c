32736fcf9806482c7b0c5cf6693e356c903989ec
SiruRoah <hammadahidii@gmail.com>
2022-07-18 11:25:00 +0100

    second task: number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - Number of element in a list
+ * @h:pointer to the head of the list
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+const listint_t *current  = h;
+int number_node = 0;
+while (current != NULL)
+{
+current  = current->next;
+number_node++;
+}
+return (number_node);
+}
