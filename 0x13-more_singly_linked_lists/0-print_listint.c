3ae2943653523ffc8bddc0d2d6d6e5e2f06ee794
SiruRoah <hammadahidii@gmail.com>
2022-07-18 11:19:35 +0100

    first task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all element of listint_t
+ * @h: const listint_t pointer
+ * Return: size_t, number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+const listint_t *current  = h;
+int number_node = 0;
+while (current != NULL)
+{
+printf("%d\n",current->n);
+current  = current->next;
+number_node++;
+}
+return (number_node);
+}
